68ca5a88015f6ab6d85820401058f19b219c6590
MarkoMuhonen <marko.muhonen@centria.fi>
2025-12-07 21:46:36 +0200

    Part A ready

--- Viikko4/lue_varaukset.py
+++ Viikko4/lue_varaukset.py
@@ -38,10 +38,21 @@ def muunna_varaustiedot(varaus: list) -> list:
     muutettu_varaus.append(bool(varaus[8]))
     muutettu_varaus.append(str(varaus[9]))
     muutettu_varaus.append(datetime.strptime(varaus[10], "%Y-%m-%d %H:%M:%S"))
-         
-        
-    return muutettu_varaus
-
+    
+    return [int(varaus[0]),
+        str(varaus[1]),
+        str(varaus[2]),
+        str(varaus[3]),
+        datetime.strptime(varaus[4], "%Y-%m-%d").date(),
+        datetime.strptime(varaus[5], "%H:%M").time(),
+        int(varaus[6]),
+        float(varaus[7]),
+        bool(varaus[8]),
+        str(varaus[9]),
+        datetime.strptime(varaus[10], "%Y-%m-%d %H:%M:%S")
+    ]
+            
+     
 def hae_varaukset(varaustiedosto: str) -> list:
     # HUOM! Tälle funktioille ei tarvitse tehdä mitään!
     # Jos muutat, kommentoi miksi muutit
